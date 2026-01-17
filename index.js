a88601816176bf427d0779c54cbe57c60d3ad686
kaifsk102 <kaifsk102@gmail.com>
2026-01-17 20:37:39 +0530

    edit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("./src/config/mongoose");
 mongoose.connect();
 
 const server = isAppSocketIOEnable ? socket : app;
-
+const PORT = process.env.PORT || 3001;
 server.listen(port, "0.0.0.0", () =>
   console.log(`Server started on port ${port} (${env})`)
 );
